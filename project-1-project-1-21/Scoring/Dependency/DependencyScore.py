8256e1364b6a50c14b583459352289dec96c5332
justinlukowski <jlukowsk@purdue.edu>
2021-11-08 14:38:28 -0500

    slight modification

--- project-1-project-1-21/Scoring/Dependency/DependencyScore.py
+++ project-1-project-1-21/Scoring/Dependency/DependencyScore.py
@@ -4,9 +4,11 @@ from Scoring.ScoreBaseClass import ScoreBaseClass
 from Scoring.Dependency.Clone import clone_module
 
 class DependencyScore(ScoreBaseClass):
-    package = None
+    url = None
+    name = None
     def __init__(self, package):
-        self.package = package
+        self.url = package.html_url
+        self.name = package.full_name
         super().__init__(package)
 
     def score(self):
@@ -14,5 +16,5 @@ class DependencyScore(ScoreBaseClass):
         # print(self.package.html_url)
         # print(self.package.url)
         # print(get_issues(self.package.full_name))
-        score = clone_module(self.package.html_url, self.package.full_name)
+        score = clone_module(self.url, self.name)
         return score

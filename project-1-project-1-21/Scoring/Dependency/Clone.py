51a8928c22448888f474070d862496ab8d5482c9
justinlukowski <jlukowsk@purdue.edu>
2021-11-08 14:28:42 -0500

    dependency score calculation

---
+++ project-1-project-1-21/Scoring/Dependency/Clone.py
@@ -0,0 +1,43 @@
+import os
+from git import Repo
+from git import rmtree
+import json
+import re
+
+
+def clone_module(url, module_name):
+    # Check if the repo already is cloned, if not then clone
+    repo = Repo.clone_from(url, module_name)  # could maybe use giturl but we dont have that yet
+
+    # Analyze cloned repository
+    score = module_clone_readme_analyzer(module_name)
+
+    # Remove cloned repo
+    cwd = os.getcwd()
+    directory_folder_empty = os.path.join(cwd, module_name.split("/")[0])
+    rmtree(directory_folder_empty)
+    return score
+
+
+def module_clone_readme_analyzer(module_name):
+    cwd = os.getcwd()
+    directory = os.path.join(cwd, module_name)
+    count = 0
+    total = 0
+    for filename in os.listdir(directory):
+        if filename == "package.json":
+            name = directory + "/package.json"
+            file = open(name)
+            data = json.load(file)
+            for key, value in data["dependencies"].items():
+                p = re.compile("[~^<>]?[=]?\d+\.\d+\.?[X|x|\d+]?")
+                x = p.search(value)
+                if x is not None:
+                    count += 1
+                #    print(x.group(), value, "yes")
+                #else:
+                #    print(value, "no")
+                total += 1
+    score = count/total
+    #print(score)
+    return score
--- project-1-project-1-21/Scoring/Dependency/DependencyScore.py
+++ project-1-project-1-21/Scoring/Dependency/DependencyScore.py
@@ -1,10 +1,18 @@
 # NEW
 
 from Scoring.ScoreBaseClass import ScoreBaseClass
+from Scoring.Dependency.Clone import clone_module
 
 class DependencyScore(ScoreBaseClass):
+    package = None
     def __init__(self, package):
+        self.package = package
         super().__init__(package)
 
     def score(self):
-        return 99999
+        # print(self.package.full_name)
+        # print(self.package.html_url)
+        # print(self.package.url)
+        # print(get_issues(self.package.full_name))
+        score = clone_module(self.package.html_url, self.package.full_name)
+        return score

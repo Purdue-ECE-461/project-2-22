95585317461188dbb798bb57dc265e2054447f16
alia-ahmed22 <aliaahmed@LAPTOP-Q5O26HM8.localdomain>
2021-11-29 11:21:08 -0500

    lol

--- test_flaskr.py
+++ test_flaskr.py
@@ -35,12 +35,5 @@ def test_run():
     assert True
 
 
-def test_open_socket():
-    result = subprocess.run(['./cloud_sql_proxy', '-instances=ece-461-project-2-22:us-east1:proj-2-database-mysql=tcp'
-                                                  ':3306'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print(result.stderr)
-    print(result.stdout)
-
-
 if __name__ == '__main__':
     print("fun")

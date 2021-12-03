3ad6c92090ba4cab47dc8d582f4e0522b848225e
alia-ahmed22 <aliaahmed@LAPTOP-Q5O26HM8.localdomain>
2021-12-03 16:15:19 -0500

    Temp file testing

--- Actions/Decode.py
+++ Actions/Decode.py
@@ -15,9 +15,11 @@ def decode_base64(output_zip, string_to_decode):
         f.write(decoded)
 
 
+#todo return the correct filename/path
 def string_to_text_file(encoded_text, text_file_folder_path, filename_original):
-    filename = f"tmp/{filename_original}.txt"
-    file_path = os.path.join(text_file_folder_path, filename)
+    filename = f"{filename_original}.txt"
+    text_file_folder_path = os.path.join(text_file_folder_path, "tmp")
+    file_path = os.path.join('/tmp', filename)
     with open(file_path, 'w') as f:
         f.write(encoded_text)
     return file_path, filename

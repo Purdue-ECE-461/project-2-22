f23ba4211d5b300b21577aaff8f6ad76063577a5
alia-ahmed22 <aliaahmed@LAPTOP-Q5O26HM8.localdomain>
2021-12-05 21:17:18 -0500

    Fixing Tests

--- test_flaskr.py
+++ test_flaskr.py
@@ -3,18 +3,17 @@ import send_requests
 
 
 def test_post_rate():
-    send_requests.ping_post_package('Cloudier', '3.1.2', '68', 'https://github.com/cloudinary/cloudinary_npm', '',
+    res = send_requests.ping_post_package('Cloudier', '3.1.2', '68', 'https://github.com/cloudinary/cloudinary_npm', '',
                                           'cloudy with a chance of meatballs')
-    p_id = (database_helper.get_package_id('Cloudier', '3.1.2', '68'))
+    p_id = res[2]['ID']
     res = send_requests.ping_rate_package_by_id(p_id)
     assert res[2]['BusFactor'] == 0.5
 
 
 def test_get_packages():
     resp = send_requests.ping_get_packages('^3.1.2', 'Cloudier')
-    k_id = (database_helper.get_package_id('Cloudier', '3.1.2', '68'))
-    pid = resp[2][0]['ID']
-    if pid == k_id:
+    packages = len(resp[2])
+    if len(packages) > 0:
         assert True
     else:
         assert False
@@ -24,11 +23,11 @@ def test_delete_package_by_name():
     resp = send_requests.ping_delete_package_by_name('Cloudier')
     if resp[1] != 200:
         assert False
-    packages = database_helper.get_all_packages()
-    for p in packages:
-        if p[0] == 'Cloudier':
-            assert False
-    assert True
+    packages = send_requests.ping_get_packages('3.1.2', 'Cloudier')[2]
+    if len(packages) > 0:
+        assert False
+    else:
+        assert True
 
 
 def test_run():

792b567f0363a766dda1190ce68912c5fcaf428e
sguadav <sguada@purdue.edu>
2021-12-08 09:48:45 -0500

    Changes to Update and Download, check for file

--- Actions/Update.py
+++ Actions/Update.py
@@ -1,9 +1,11 @@
 from Actions import Delete
 from Actions import Upload
-from Actions import ActionHelper
+# import Delete
+# import Upload
 
 
-def update_file(bucket_name, object_name, content):
-    Delete.delete_object(bucket_name, object_name + '.txt')
-    Upload.upload_file(object_name, content, bucket_name)
+def update_file(bucket_name, encoded_zipfile_string, filename_to_gcp):
+    Delete.delete_object(bucket_name, filename_to_gcp)
+    Upload.upload_file(filename_to_gcp, encoded_zipfile_string, bucket_name)
+    return
 
--- Actions/Upload.py
+++ Actions/Upload.py
@@ -5,6 +5,8 @@ def upload_file(filename_to_gcp, encoded_zipfile_string, destination_bucket_gcp)
     # filename_to_gcp: the name of the file that is going to be in gcp
     client = storage.Client()
     bucket = client.get_bucket(destination_bucket_gcp)
+    if len(filename_to_gcp.split(".")) == 1:
+        filename_to_gcp = filename_to_gcp + ".txt"
     blob = bucket.blob(filename_to_gcp + ".txt")
     blob.upload_from_string(encoded_zipfile_string)
 

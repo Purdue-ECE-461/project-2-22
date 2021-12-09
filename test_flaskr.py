0e1b91e49629a1bce586d79d9994dfe5ded46737
Alia Ahmed <ahmed119@purdue.edu>
2021-12-08 20:07:02 -0500

    Update test_flaskr.py

--- test_flaskr.py
+++ test_flaskr.py
@@ -33,7 +33,7 @@ def test_empty_package():
 
 def test_delete_package_by_id():
     res = send_requests.ping_post_package_no_data(TEST_PACKAGE_NAME, '11.11.11', '78', '')
-    p_id = int(res[1])
+    p_id = res[2][0]
 
     print(p_id)
 
